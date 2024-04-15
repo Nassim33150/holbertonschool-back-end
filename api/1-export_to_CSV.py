2ba3b57a47a8035eacd258f6c64f68c126a0ac6f
root <root@LAPTOP-2LV4F0PM>
2024-04-15 16:11:42 +0200

    export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,19 +5,24 @@ import csv
 
 if __name__ == "__main__":
     Id = sys.argv[1]
-    
+
     """ get userinformations with request"""
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(Id))
-    
+
     """ get username """
     username = user.json().get('name')
-    
+
     """ get all usertasks """
     tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
-     
+
     with open(f"{Id}.csv", mode='w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for task in tasks.json():
             if task.get('userId') == int(Id):
-                writer.writerow([f'"{Id}"', f'"{username}"', f'"{task.get("completed")}"', f'"{task.get("title")}"'])
+                writer.writerow([
+                    f'"{Id}"',
+                    f'"{username}"',
+                    f'"{task.get("completed")}"',
+                    f'"{task.get("title")}"'
+                ])

c286d1586433396a08a6a22e08fb7ecc598be351
root <root@LAPTOP-2LV4F0PM>
2024-04-17 11:33:20 +0200

    export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+""" Records all tasks that are owned by this employee """
 import sys
 import requests
 import csv
 
 if __name__ == "__main__":
-    Id = sys.argv[1]
+    USER_ID = sys.argv[1]
 
     """ get userinformations with request"""
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(Id))
+                        .format(USER_ID))
 
     """ get username """
-    username = user.json().get('name')
+    EMPLOYEE_NAME = user.json().get('name')
 
     """ get all usertasks """
     tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    with open(f"{Id}.csv", mode='w', newline='') as csvfile:
+    with open(f"{USER_ID}.csv", mode='w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for task in tasks.json():
-            if task.get('userId') == int(Id):
+            if task.get('userId') == int(USER_ID):
+                TASK_COMPLETED_STATUS = task.get('completed')
+                TASK_TITLE = task.get('title')
                 writer.writerow([
-                    f'"{Id}"',
-                    f'"{username}"',
-                    f'"{task.get("completed")}"',
-                    f'"{task.get("title")}"'
+                    f'"{USER_ID}"',
+                    f'"{EMPLOYEE_NAME}"',
+                    f'"{TASK_COMPLETED_STATUS}"',
+                    f'"{TASK_TITLE}"'
                 ])
